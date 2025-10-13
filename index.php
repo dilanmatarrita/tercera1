0d1cf1741e041af14813a122e11d995cd67550cb
dilanmatarrita <matarritachacon@gmail.com>
2025-10-13 10:11:04 -0600

    Creacion de proyecto

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php require_once "./vistas/vista_superior.php"?>
---
+++ vistas/vista_superior.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="Español">
+
+<head>
+<meta charset="utf-8">
+<meta name="Tercera 1" content="Proyecto de Tercera 1">
+<title>Tercera 1</title>
+<link rel="stylesheet" href="./css/main.css">
+
+</head>
