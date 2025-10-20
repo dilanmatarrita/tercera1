49c32a341e6315ad9a496cbac1d11c06252b9ffe
dilanmatarrita <matarritachacon@gmail.com>
2025-10-19 19:10:25 -0600

    domingo 2

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,34 +1,34 @@
 
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="utf-8">
-  <meta name="Tercera 1" content="Proyecto de Tercera 1">
-  <title>Tercera 1</title>
-  <link rel="stylesheet" href="../css/main.css">
-</head>
-<menu>
+<!DOCTYPE html> 
+<html lang="Español"> 
+<head> 
+<meta charset="utf-8"> 
+<meta name="Tercera 1" content="Proyecto de Tercera 1"> 
+<title>Tercera 1</title> 
+<link rel="stylesheet" href="./css/main.css"> 
+</head> 
 
-  <div class="menu">
-       <div class="menu_izquierdo"> 
-         <img class="logo" src="../img/fondo.png" alt="fondo">    
-       </div>    
-
-       <div class="menu_derecho">
-         <div class="hamburger-icon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
-       <div class="menu_normal">
-         <a href="./">Inicio</a>   
-         <a href="./shonen.php">Shonen</a> 
-         <a href="./figuras.php">Figuras</a> 
-         <a href="./alimentos.php">Alimentos</a>  
-         <a href="./contacto.php">Contacto</a>
-         </div> 
-       </div>  
-    </div> 
+<menu> 
+        <div class="menu_izquierdo"> 
+            <img class="logo" src="./img/fondo.png" alt="fondo"> 
+        </div> 
+     
+    <div class="menu_derecho"> 
+        <div class="menu-container"> 
+           <div class="hamburger-icon"> 
+              <span></span> 
+              <span></span> 
+              <span></span> 
+            </div> 
+    
+           <div class="menu_normal"> 
+              <a href="./">inicio</a> 
+              <a href="./shonen.php">shonen</a> 
+              <a href="./figuras.php">figuras</a> 
+              <a href="./alimentos.php">alimentos</a> 
+              <a href="./contacto.php">contacto</a> 
+           </div> 
+        </div> 
+   </div> 
 </menu>
